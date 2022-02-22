5cf7222925b263af62cb1894e30be4474e285dd9
WashingtonYandun <washington.yandun06@gmail.com>
2022-02-22 17:48:32 -0500

    start making the navbar (working on responsive)

--- front_react/src/components/Navbar/Navbar.jsx
+++ front_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { images } from "../../constants";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src="" alt="" />
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <span>WMYM</span>
+      </div>
+      <div className="app__navbar-links">
+        {["home", "about", "skills", "works", "contact"].map((item) => {
+          return (
+            <a
+              key={`link-${item}`}
+              className="app__flex p-text"
+              href={`#${item}`}
+            >
+              {item}
+            </a>
+          );
+        })}
       </div>
     </nav>
   );
---
+++ front_react/src/constants/images.js
@@ -0,0 +1,31 @@
+import email from "../assets/email.png";
+import circle from "../assets/circle.svg";
+import logo from "../assets/logo.png";
+
+import api from "../assets/api.png";
+
+import git from "../assets/git.png";
+import cpp from "../assets/cpp.png";
+import css from "../assets/css.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import redux from "../assets/redux.png";
+
+export default {
+  email,
+  api,
+  cpp,
+  css,
+  git,
+  html,
+  javascript,
+  node,
+  python,
+  react,
+  redux,
+  circle,
+  logo,
+};
