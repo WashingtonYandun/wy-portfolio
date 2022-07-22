96c0522f0b698a755d2e407978e66a4434f7ff5d
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-21 23:53:27 -0500

    add portfolio component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Portfolio from "./components/portfolio/Portfolio";
+import About from "./components/about/About";
+import NavBar from "./components/navbar/NavBar";
+import Experience from "./components/experience/Experience";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-    return <div>lorem ipson dolor dfafdsfadfadf</div>;
+    return (
+        <>
+            <Header></Header>
+            <NavBar></NavBar>
+            <About></About>
+            <Experience></Experience>
+            <Portfolio></Portfolio>
+            <Contact></Contact>
+            <Footer></Footer>
+        </>
+    );
 };
 
 export default App;
