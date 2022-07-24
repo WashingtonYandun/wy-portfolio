ca237d4e92d0e22588d713af9bc391f2e200b96c
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-24 00:55:09 -0500

    finish about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,4 @@
-import { GrGrow } from "react-icons/gr";
+import { MdOutlineWorkOutline } from "react-icons/md";
 import { BsCodeSlash } from "react-icons/bs";
 import { SiFuturelearn } from "react-icons/si";
 
@@ -20,33 +20,33 @@ const About = () => {
                     <div className="about__content">
                         <div className="about__cards">
                             <article className="about__card">
-                                <GrGrow className="about__cards-icon" />
+                                <MdOutlineWorkOutline className="about__cards-icon" />
                                 <h5>Experience</h5>
-                                <small>2+ Months Working</small>
+                                <small>2+ Months</small>
                             </article>
                             <article className="about__card">
                                 <SiFuturelearn className="about__cards-icon" />
                                 <h5>Courses</h5>
-                                <small>5+ Completed</small>
+                                <small>5+ Done</small>
                             </article>
                             <article className="about__card">
                                 <BsCodeSlash className="about__cards-icon" />
                                 <h5>Projects</h5>
                                 <small>40+ Repos</small>
                             </article>
-                            <p>
-                                I am a Software Engineering student, who loves
-                                his degree. The technologies in which most I
-                                highlight are Java, C#, JavaScript, React and
-                                Python, I am a responsible and committed person
-                                in his work, besides that I am in constant
-                                learning since I love to learn and work.
-                            </p>
-
-                            <a href="#contact" className="btn btn-primary">
-                                Talk to me
-                            </a>
                         </div>
+                        <p>
+                            I am a Software Engineering student, who loves his
+                            degree. The technologies in which most I highlight
+                            are Java, C#, JavaScript, React and Python, I am a
+                            responsible and committed person in his work,
+                            besides that I am in constant learning since I love
+                            to learn and work.
+                        </p>
+
+                        <a href="#contact" className="btn btn-primary">
+                            Talk to me
+                        </a>
                     </div>
                 </div>
             </section>
