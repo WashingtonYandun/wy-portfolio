4e312be6789700772226ca70b86bca75bfbf4e33
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-23 23:09:37 -0500

    start making the AAbout section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,55 @@
+import { GrGrow } from "react-icons/gr";
+import { BsCodeSlash } from "react-icons/bs";
+import { SiFuturelearn } from "react-icons/si";
+
+import ME2 from "../../assets/ascii wmym2.jpeg";
 import "./About.css";
 
 const About = () => {
     return (
         <>
-            <section id="about">About</section>
+            <section id="about">
+                <h5>Some</h5>
+                <h2>About me</h2>
+
+                <div className="container about__container">
+                    <div className="about__me">
+                        <img src={ME2} alt="me" />
+                    </div>
+
+                    <div className="about__content">
+                        <div className="about__cards">
+                            <article className="about__card">
+                                <GrGrow className="about__cards-icon" />
+                                <h5>Experience</h5>
+                                <small>2+ Months Working</small>
+                            </article>
+                            <article className="about__card">
+                                <SiFuturelearn className="about__cards-icon" />
+                                <h5>Courses</h5>
+                                <small>5+ Completed</small>
+                            </article>
+                            <article className="about__card">
+                                <BsCodeSlash className="about__cards-icon" />
+                                <h5>Projects</h5>
+                                <small>40+ Repos</small>
+                            </article>
+                            <p>
+                                I am a Software Engineering student, who loves
+                                his degree. The technologies in which most I
+                                highlight are Java, C#, JavaScript, React and
+                                Python, I am a responsible and committed person
+                                in his work, besides that I am in constant
+                                learning since I love to learn and work.
+                            </p>
+
+                            <a href="#contact" className="btn btn-primary">
+                                Talk to me
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
