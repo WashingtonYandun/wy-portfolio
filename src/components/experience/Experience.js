7a8a4eb221175fd4eeef3a76ecfa2783d0f143c7
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-25 00:16:52 -0500

    change landing distribution

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,7 +1,14 @@
 import "./Experience.css";
 
 export const Experience = () => {
-    return <section id="experience">Experience</section>;
+    return (
+        <>
+            <section id="experience">
+                <h5>What is my Experience</h5>
+                <h2>Jobs I had</h2>
+            </section>
+        </>
+    );
 };
 
 export default Experience;
