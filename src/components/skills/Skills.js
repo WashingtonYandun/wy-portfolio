b5d0c4dd98533ef26d01784b727895c2289e7ef4
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-24 22:33:00 -0500

    add some features in skills component and solve about bug

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,3 +1,4 @@
+import { MdDoneAll } from "react-icons/md";
 import "./Skills.css";
 
 export const Skills = () => {
@@ -10,11 +11,70 @@ export const Skills = () => {
                 <div className="container skills__container">
                     <div className="skills__tech">
                         <h3>Technical Skills</h3>
-                        <div className="skills__content"></div>
+                        <div className="skills__content">
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>HTML/CSS</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>JavaScript</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>React.js</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>C++</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Python</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Java</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>C#</h4>
+                            </article>
+                        </div>
                     </div>
+
                     <div className="skills__no-tech">
-                        <h3>No Technical Skills</h3>
-                        <div className="skills__content"></div>
+                        <h3>Extra Skills</h3>
+                        <div className="skills__content">
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Project Managment</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Git/Github</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>SQL/MongoDB</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>npm/pip</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Linux Essentials</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Automating Tasks</h4>
+                            </article>
+                            <article className="skills__details">
+                                <MdDoneAll />
+                                <h4>Problem Solving</h4>
+                            </article>
+                        </div>
                     </div>
                 </div>
             </section>
