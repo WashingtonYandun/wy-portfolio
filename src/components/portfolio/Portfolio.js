27ff16a1b246c7699aa9fcc746a7bc1f95db9ef8
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-26 21:55:35 -0500

    start implementing the portfolio section (component)

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,25 @@
 import "./Portfolio.css";
 
 const Portfolio = () => {
-    return <section id="portfolio">Portfolio</section>;
+    return (
+        <>
+            <section id="portfolio">
+                <h5>Most Important works</h5>
+                <h2>Portfolio</h2>
+                <div className="container portfolio__container">
+                    <article className="portfolio__project">
+                        <div className="portfolio__img">
+                            <img src="" alt="project" />
+                        </div>
+                        <h3>Neural Network</h3>
+                        <a href="https://github.com/WashingtonYandun/todo.react.js">
+                            Repo
+                        </a>
+                    </article>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Portfolio;
