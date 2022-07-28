a51106bd655a413bc6309679fbac491d6e1c5b6c
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-27 23:46:15 -0500

    use a map with an array for improving scalability in projects

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,81 +1,64 @@
 import "./Portfolio.css";
 
 const Portfolio = () => {
+    // using an array of objects to display the portfolio projects
+    const projects = [
+        {
+            id: 1,
+            title: "Farenheit Celsius AI",
+            image: "https://raw.githubusercontent.com/WashingtonYandun/CelciusAndFahrenheit_AI.py/main/img/sample.png",
+            repo: "https://github.com/WashingtonYandun/CelciusAndFahrenheit_AI.py",
+        },
+        {
+            id: 2,
+            title: "Todo made in React.js",
+            image: "https://raw.githubusercontent.com/WashingtonYandun/todo.react.js/main/sample.png",
+            repo: "https://github.com/WashingtonYandun/todo.react.js",
+        },
+        {
+            id: 3,
+            title: "Snake game made in Java",
+            image: "https://raw.githubusercontent.com/WashingtonYandun/Snake.java/main/samples/playing.png",
+            repo: "https://github.com/WashingtonYandun/CelciusAndFahrenheit_AI.py",
+        },
+        {
+            id: 4,
+            title: "Art class portfolio",
+            image: "https://raw.githubusercontent.com/WashingtonYandun/Art-Human-Expression-Communication_Portfolio/main/sample.png",
+            repo: "https://github.com/WashingtonYandun/CelciusAndFahrenheit_AI.py",
+        },
+    ];
+
     return (
         <>
             <section id="portfolio">
                 <h5>Most Important works</h5>
                 <h2>Portfolio</h2>
                 <div className="container portfolio__container">
-                    <article className="portfolio__project">
-                        <div className="portfolio__img">
-                            <img
-                                src="https://raw.githubusercontent.com/WashingtonYandun/CelciusAndFahrenheit_AI.py/main/img/sample.png"
-                                alt="Celcius Farenheit AI prediction"
-                            />
-                        </div>
-                        <div>
-                            <h3>Farenheit Celsius AI</h3>
-                            <a
-                                href="https://github.com/WashingtonYandun/CelciusAndFahrenheit_AI.py"
-                                className="btn btn-primary"
+                    {projects.map((project) => {
+                        return (
+                            <article
+                                key={project.id}
+                                className="portfolio__project"
                             >
-                                See it
-                            </a>
-                        </div>
-                    </article>
-
-                    <article className="portfolio__project">
-                        <div className="portfolio__img">
-                            <img
-                                src="https://raw.githubusercontent.com/WashingtonYandun/todo.react.js/main/sample.png"
-                                alt="todo react"
-                            />
-                        </div>
-                        <div>
-                            <h3>Todo made in React.js</h3>
-                            <a
-                                href="https://github.com/WashingtonYandun/todo.react.js"
-                                className="btn btn-primary"
-                            >
-                                See it
-                            </a>
-                        </div>
-                    </article>
-
-                    <article className="portfolio__project">
-                        <div className="portfolio__img">
-                            <img
-                                src="https://raw.githubusercontent.com/WashingtonYandun/Snake.java/main/samples/playing.png"
-                                alt="todo react"
-                            />
-                        </div>
-                        <h3>Snake game made in Java</h3>
-                        <a
-                            href="https://github.com/WashingtonYandun/todo.react.js"
-                            className="btn btn-primary"
-                        >
-                            See it
-                        </a>
-                    </article>
-
-                    <article className="portfolio__project">
-                        <div className="portfolio__img">
-                            <img
-                                src="https://raw.githubusercontent.com/WashingtonYandun/Art-Human-Expression-Communication_Portfolio/main/sample.png"
-                                alt="todo react"
-                            />
-                        </div>
-                        <div>
-                            <h3>Art class Portfolio</h3>
-                            <a
-                                href="https://github.com/WashingtonYandun/todo.react.js"
-                                className="btn btn-primary"
-                            >
-                                See it
-                            </a>
-                        </div>
-                    </article>
+                                <div className="portfolio__img">
+                                    <img
+                                        src={project.image}
+                                        alt={project.title}
+                                    />
+                                </div>
+                                <div>
+                                    <h3>{project.title}</h3>
+                                    <a
+                                        href={project.repo}
+                                        className="btn btn-primary"
+                                    >
+                                        See it
+                                    </a>
+                                </div>
+                            </article>
+                        );
+                    })}
                 </div>
             </section>
         </>
