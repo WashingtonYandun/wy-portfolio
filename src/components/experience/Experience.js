8122f2f1866cf18d1bb27a1ab548fe59cfa968ff
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-28 21:38:32 -0500

    refactor experience component making it more scallable

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -3,74 +3,61 @@ import "./Experience.css";
 
 export const Experience = () => {
     //this component must be refactored
+    const typeOfExperience = [
+        {
+            id: 1,
+            title: "Project Management",
+            list: {
+                1: "Project Management",
+                2: "Project Management",
+                3: "Project Management",
+            },
+        },
+        {
+            id: 2,
+            title: "Artificial Intelligence",
+            list: {
+                1: "Looking for more",
+                2: "Looking for more",
+                3: "Looking for more",
+            },
+        },
+        {
+            id: 3,
+            title: "Web Development",
+            list: {
+                1: "Looking for more",
+                2: "Looking for more",
+                3: "Looking for more",
+            },
+        },
+    ];
+
     return (
         <>
             <section id="experience">
                 <h5>What is my Experience</h5>
                 <h2>Jobs I had</h2>
                 <div className="container experience__container">
-                    <article className="experience">
-                        <div className="experience__head">
-                            <h3>Project Managment</h3>
-                        </div>
-
-                        <ul className="experience__list">
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Project Managment trainee</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                        </ul>
-                    </article>
-
-                    <article className="experience">
-                        <div className="experience__head">
-                            <h3>Artificial Intelligence</h3>
-                        </div>
-
-                        <ul className="experience__list">
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                        </ul>
-                    </article>
-
-                    <article className="experience">
-                        <div className="experience__head">
-                            <h3>Web Development</h3>
-                        </div>
-
-                        <ul className="experience__list">
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                            <li>
-                                <MdDoneAll className="experience__icon" />
-                                <p>Looking for more</p>
-                            </li>
-                        </ul>
-                    </article>
+                    {typeOfExperience.map((type) => {
+                        return (
+                            <article key={type.id} className="experience">
+                                <div className="experience__head">
+                                    <h3>{type.title}</h3>
+                                </div>
+                                <ul className="experience__list">
+                                    {Object.keys(type.list).map((key) => {
+                                        return (
+                                            <li key={key}>
+                                                <MdDoneAll className="experience__icon" />
+                                                <p>{type.list[key]}</p>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </article>
+                        );
+                    })}
                 </div>
             </section>
         </>
