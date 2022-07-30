772bf52f96dd741b61c91ff44ad2b5bcb2da28bb
WashingtonYandun <washington.yandun06@gmail.com>
2022-07-30 02:56:40 -0500

    finish all the project

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,49 @@
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { VscGithub } from "react-icons/vsc";
 import "./Footer.css";
 
 const Footer = () => {
-    return <section id="footer">Footer</section>;
+    const socialNetworks = [
+        {
+            id: 1,
+            description: "GitHub",
+            icon: <VscGithub />,
+            url: "https://github.com/WashingtonYandun",
+        },
+        {
+            id: 2,
+            description: "LinkedIn",
+            icon: <AiOutlineLinkedin />,
+            url: "https://github.com/WashingtonYandun",
+        },
+    ];
+    return (
+        <>
+            <footer>
+                <div className="footer__container">
+                    <a href="#home" className="footer__title">
+                        <h5>WMYM</h5>
+                    </a>
+                    <div className="footer__socials">
+                        {socialNetworks.map((network) => {
+                            return (
+                                <a
+                                    key={network.id}
+                                    href={network.url}
+                                    description={network.description}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="footer__social"
+                                >
+                                    {network.icon}
+                                </a>
+                            );
+                        })}
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
 };
 
 export default Footer;
